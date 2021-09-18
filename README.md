e716772d85d26bc3a4de12b8d064e17123c76949
Leslie Lopez <leslie2515lopez@gmail.com>
2021-09-17 21:14:17 -0500

    Added a ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Responsive-Website
-A beginner responsive website using HTML, CSS, and JavaScript.
+<h1>Color Gradients :art:</h1>
 
+<h2>A color gradient website using flexbox, grid, and some positioning elements.</h2>
+
+<p>This website is responsive using flexbox, grid, and some positioning elements. I followed along with a tutorial that is beginner friendly. I changed all the colors from the original tutorial, added a logo that changes color when you hover over it, added a hover effect to change color to all the buttons, footer links and social icons. I've also added JavaScript function to the copyright date so that it updates automatically every year.</p>
+
+### Screenshots
+
+<img src="src/images/desktop.png" width="200"> 
+<img src="src/images/navbar.png" width="200">
+<img src="src/images/mobile.png" width="200">
+
+### Link
+
+- Live Site: [Color Gradients](https://leslielopez25.github.io/Responsive-Website/)
--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const highlightMenu = () => {
   const elem = document.querySelector(".highlight");
   const homeMenu = document.querySelector("#home-page");
   const aboutMenu = document.querySelector("#about-page");
-  const serviceMenu = document.querySelector("#services-page");
+  const servicesMenu = document.querySelector("#services-page");
   let scrollPos = window.scrollY;
 
   // adds 'highlight' class to my menu items
@@ -29,7 +29,7 @@ const highlightMenu = () => {
     servicesMenu.classList.remove("highlight");
     return;
   } else if (window.innerWidth > 960 && scrollPos < 2345) {
-    serviceMenu.classList.add("highlight");
+    servicesMenu.classList.add("highlight");
     aboutMenu.classList.remove("highlight");
     return;
   }
@@ -42,7 +42,7 @@ const highlightMenu = () => {
 window.addEventListener("scroll", highlightMenu);
 window.addEventListener("click", highlightMenu);
 
-// Close mobile menu when clicking on a menu item
+//  Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
   const menuBars = document.querySelector(".is-active");
   if (window.innerWidth <= 768 && menuBars) {
